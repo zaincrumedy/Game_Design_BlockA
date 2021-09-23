0ef2114031ae037e8618f1e165cfae7851603c1b
zaincrumedy <darknightz062305@gmail.com>
2021-09-22 22:07:39 -0500

    9/22 homework

--- smallgame.py
+++ smallgame.py
@@ -1,16 +1,40 @@
 import os
 import random
 
+os.system('cls')
+
 for i in range(100):
     randy= random.randint(1,100)
-    print(randy)
+    #print(randy)
 
-print("guess the number, you have 10 tries")
+print("This is a random number guessing game, you have 10 tries to get the number right. I will give you hints if the number is higher or lower. Good luck!")
 
-if(randy-guess>25):
-    print("too high")
 
-else:
-    (guess - randy>25):
-    print("too high")
+for i in range(1,11):
+    userInput=input("Guess the number ")
+    try:
+        userInput=int(userInput)
+        check = True 
+    except ValueError:
+        check=False 
 
+    if (randy==userInput):
+        print("You win!!")
+        
+    else:
+        print("try again")
+
+    if (randy<userInput):
+        print("too high")
+
+    else:
+        (randy>userInput)
+        print("too low")
+        
+
+# if(randy- i >1):
+#     print("too high, guess again")
+
+# else:
+#     (i - randy>1)
+#     print("too high, guess again")
