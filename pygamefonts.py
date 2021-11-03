0765b79643a33798d0b10ccda04b4487fac38d05
zaincrumedy <darknightz062305@gmail.com>
2021-11-03 18:52:44 -0500

    push6

--- pygamefonts.py
+++ pygamefonts.py
@@ -8,9 +8,11 @@ WHITE=(0,0,0)
 PURPLE=(150,0,150)
 WIDTH = 800
 HEIGHT = 800
+bmessages=["Back"]
 messages=['Instructions','Level 1','Level 2','Settings','Score Board', 'Exit']
-messages2=['Screen size', 'Object Color', 'Sound on/off', 'Back']
-Ssmessages=['Larger', 'Smaller', 'Back']
+messages2=['Screen size', 'Object Color', 'Sound on/off', ]
+Ssmessages=['Larger', 'Smaller', ]
+imessages=["do this to win"]
 win=py.display.set_mode((WIDTH,HEIGHT))
 py.display.set_caption("Setting Window")
 #TITLE_FONT= py.font.SysFont(name,size,bold=false, italic= false)
@@ -34,6 +36,21 @@ def displaySub(subTitle,ysub):
     py.display.update()
     py.time.delay(10)
 
+def display_back():
+                x=660
+                y=730
+                square.x=x
+                square.y=y
+                for i in range(0,len(bmessages)):
+                    word= bmessages[i]
+                    py.draw.rect(win, PURPLE, square)
+                    text=SUBTITLE_FONT.render(word,10, BLACK)
+                    win.blit(text, (x+wbox+10, y))
+                    py.display.flip()
+                    
+                    
+                    square.y=y
+
 
 def display_Menu():
     x=70
@@ -56,9 +73,12 @@ display_message('Menu')
 ysub=200
 py.time.delay(200)
 display_Menu()
+display_back()
 run=True
-
+counter=1
+back=1
 while run:
+    
     for eve in py.event.get():
         if eve.type == py.QUIT:
             run=False
@@ -68,11 +88,12 @@ while run:
         if mouse_pressed[0]:
             mouse_pos=py.mouse.get_pos()
             print(mouse_pos)
-            if mouse_pos[0]>=70 and mouse_pos[0]<=95 and mouse_pos[1]>490 and mouse_pos[1]<515:
+            if mouse_pos[0]>=70 and mouse_pos[0]<=95 and mouse_pos[1]>490 and mouse_pos[1]<515 and counter==1:
                 win.fill(WHITE)
                 display_message("Settings")
                 py.display.set_caption("Setting Window")
                 mouse_pos=py.mouse.get_pos()
+                counter +=1
                 
                 x=70
                 y=190
@@ -87,8 +108,42 @@ while run:
                     py.time.delay(100)
                     y += 100
                     square.y=y
+
+                x=660
+                y=730
+                square.x=x
+                square.y=y
+                for i in range(0,len(back)):
+                    word= back[i]
+                    py.draw.rect(win, PURPLE, square)
+                    text=SUBTITLE_FONT.render(word,10, BLACK)
+                    win.blit(text, (x+wbox+10, y))
+                    py.display.flip()
+                    
+                    
+                    square.y=y            
+
+            if mouse_pos[0]>70 and mouse_pos[0]<=95 and mouse_pos[1]>185 and mouse_pos[1]<=215 and counter==1:
+                win.fill(WHITE)
+                display_message('Instructions')
+                py.display.set_caption("instructions window")
+                x=70
+                y=190
+                square.x=x
+                square.y=y
+                
+                for i in range(0, len(imessages)):
+                    word= imessages[i]
+                    py.draw.rect(win, PURPLE, square)
+                    text=SUBTITLE_FONT.render(word,10, BLACK)
+                    win.blit(text, (x+wbox+10, y))
+                    py.display.flip()
+                    py.time.delay(100)
+                    y += 100
+                    square.y=y
             
-            if mouse_pos[0]>70 and mouse_pos[0]<=95 and mouse_pos[1]>185 and mouse_pos[1]<=215:
+            
+            if mouse_pos[0]>70 and mouse_pos[0]<=95 and mouse_pos[1]>185 and mouse_pos[1]<=215 and counter==2:
                 win.fill(WHITE)
                 display_message('Screen Size')
                 py.display.set_caption("Screen Size Window")
@@ -105,9 +160,21 @@ while run:
                     py.time.delay(100)
                     y += 100
                     square.y=y
+
             
-            
+              
+                    
+                            
                 
+            
+               
+          
+                    
+                            
+                                
+
+            print(counter)
+            print(back)
 
             
                 
